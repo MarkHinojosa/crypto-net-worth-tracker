57805ccdad524d807ca60d84aefb04a8a1403473
mark <markhinojosa1212@gmail.com>
2021-07-21 21:34:30 -0500

    added submit for ETH

--- src/components/wallet/Wallet.js
+++ src/components/wallet/Wallet.js
@@ -1,6 +1,54 @@
 import React from 'react';
 
+
+
 export default class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={ 
+      ethValue: "", 
+      btcValue: "",
+      ethAddresses: [],
+      btcAddresses: []
+    }
+   }
+
+  ethHandleSubmit = (event) => {
+    console.log(event)
+    event.preventDefault();
+    this.setState({ethAddresses: [...this.state.ethAddresses, this.state.ethValue], ethValue: ""},
+    ()=> console.log(this.state, "the state ***"))
+  }
+
+  ethHandleChange = (event) => {
+    console.log(event.target.value, " event ")
+    this.setState({ ethValue: event.target.value }, () => console.log(this.state))
+  }
+
+  ethLabel = () => {
+    return (
+      <form onSubmit={this.ethHandleSubmit}>
+      <label>
+        ETH Wallet Address:
+        <input type="text" value={this.state.ethValue} onChange={this.ethHandleChange} />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+    )
+  }
+
+  btcLabel = () => {
+    return (
+      <form onSubmit={this.handleSubmit}>
+      <label>
+        BTC Wallet Address:
+        <input type="text" value={this.state.btcValue} onChange={this.handleChange} />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+    )
+  }
+
   render () {
     return (
       <div style={{
@@ -18,6 +66,7 @@ export default class Wallet extends React.Component {
         <div>
           <p>ETH wallet</p>
           <p>.01234567</p>
+          {this.ethLabel()}
         </div>
       </div>
     )
